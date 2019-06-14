bcb37dcaea50ce11029853b5011adcc52228fe6b
itw_fanhy <2531030188@qq.com>
2019-06-14 21:41:07 +0800

    Initial commit

---
+++ app/src/main/java/com/xzq/usercase/activity/HomeActivity.java
@@ -0,0 +1,20 @@
+package com.xzq.usercase.activity;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hope.usercase.R;
+
+public class HomeActivity extends AppCompatActivity {
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
